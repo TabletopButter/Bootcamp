587466f8da13c855ff282508da05a102ec4e18d0
TabletopButter <70969196+TabletopButter@users.noreply.github.com>
2020-10-12 22:02:49 -0500

    finished javascript project1

---
+++ javascript/project1/script.js
@@ -0,0 +1,7 @@
+var name; var fsnack; var fdrink;
+
+name = prompt("Please enter your name");
+fsnack = prompt("What is your favorite snack?");
+fdrink = prompt("What is your favorite drink");
+
+document.write("Hi there "+name+","+" it seems that you enjoy eating "+fsnack+" and drinking "+fdrink+"!");
