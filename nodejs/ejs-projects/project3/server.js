52ba9e916b990b06fe3de34a5084b8e2c2a6367b
TabletopButter <70969196+TabletopButter@users.noreply.github.com>
2020-12-01 17:22:56 -0600

    initial ejs project3

---
+++ nodejs/ejs-projects/project3/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.get('/', (req, res)=>{
+    res.render("home.ejs");
+})
+
+app.get("/about", (req, res)=>{
+    res.render("about.ejs");
+})
+
+app.get("/contact", (req, res)=>{
+    res.render("contact.ejs");
+})
+
+
+app.listen(PORT, ()=> console.log(`App listening on port ${PORT}.`));
